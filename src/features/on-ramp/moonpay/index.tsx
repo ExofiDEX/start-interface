b3676c0df7620868b1be9a6fedb937390c22a73f
Bilal Ch <thered001@gmail.com>
2022-10-12 14:12:49 +0500

    feat: added sell crypto widget

--- src/features/on-ramp/moonpay/index.tsx
+++ src/features/on-ramp/moonpay/index.tsx
@@ -1,13 +1,27 @@
 import { useLingui } from '@lingui/react'
 
-export default function Buy() {
+export function Buy() {
   const { i18n } = useLingui()
   return (
     <iframe
       allow="accelerometer; autoplay; camera; gyroscope; payment"
       frameBorder="0"
       height="100%"
-      src="https://buy-sandbox.moonpay.com?apiKey=pk_test_d11ELg96c0pv45LtJnxRuYz1WFnX7i"
+      src="https://buy-staging.moonpay.io?apiKey=pk_test_d11ELg96c0pv45LtJnxRuYz1WFnX7i"
+      width="100%"
+      scrolling="no"
+    />
+  )
+}
+
+export function Sell() {
+  const { i18n } = useLingui()
+  return (
+    <iframe
+      allow="accelerometer; autoplay; camera; gyroscope; payment"
+      frameBorder="0"
+      height="100%"
+      src="https://sell-staging.moonpay.io?apiKey=pk_test_d11ELg96c0pv45LtJnxRuYz1WFnX7i"
       width="100%"
       scrolling="no"
     />
